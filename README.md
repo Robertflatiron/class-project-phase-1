94b986cee9a1891d17a697907e9da61d16e6926d
Robert Gaspar <gaspar.robert7@gmail.com>
2021-11-20 13:39:23 -0600

    new

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-//JS issues I'm running into. Since each car's HTML is the same, I've run into the issues on applying the JS code to each car. 
-The first car's like button as well as the comments/reviews input is functioning, but I'm having difficulty getting the other cars to do the same. I changed car #2 (Subaru) button id and changed it to id="like-button-two" and created another event listener to grab that car and have its likes and comments work. 
-Right now, the subaru is compeltely set as default because once you select post (to post a comment) the page refreshes. I'm not getting errors in the DOM so I'm a little stuck. 
-I also had to change the image source in the HTML. It was under ./assests and then the car name, but we have the ./assests right now so it wouldn't pull the image. You're more than welcome to change anything you need.
+First eventlistener is the like button
+Second eventlistener is the comments
+Added a third eventlistener (title name turns red when clicked)
 
-Added a third eventlistener (title name turns red when clicked).
+Hey Dan, I got the likes to work on each car, the comments work on each car, also grabbed the car name from the API to change the name of each car. Since the API is under the Honda only make, the 6 cars are all Honda. I couldn't find a way to have the Honda as lower case because the API has them as lower case. I really am fine with them being capital, cause its working honestly. 
 
-Wasn't able to get the comments to upload. Received out to a friend and we will jump on a zoom call tomorrow and he will help me with that. After that, we need to get the make_name and model_name from the API to actually pass this project. 
+The only thing left to do, if you can change each image to correspond to the new car's name. Then we're done
 
--- test.js
+++ test.js
@@ -1,58 +1,38 @@
 const likes = document.querySelectorAll("span"); //like count
 const commentsList = document.querySelector("ul");
-const title = document.querySelector("#card-title");
+const title = document.querySelector(".card-title");
 const likeCount = document.querySelectorAll("#like-count");
 const commentForm = document.querySelectorAll("#comment-form")
 
-
-
-
 fetch("https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/honda?format=json")
 .then((response) => response.json())
-.then((carData) => (carData))
-
-
-function renderCarName(e) {
-
-  const carName = e.target.value;
-  const filteredCars = carData.results.filter((car) => {
-    return (
-      car.Model_Name * Make_Name.includes(carName));
-    })
-    console.log(filteredCars);
-}
-
+.then((carData) => {
+  const allCarTitles = document.querySelectorAll('.car-title')
+  // console.log(allCarTitles)
+  for (let i = 0; i <= 5; i++) {
+    console.log(allCarTitles[i])
+    console.log(carData.Results[i])
+    allCarTitles[i].innerHTML = `${carData.Results[i].Make_Name} ${carData.Results[i].Model_Name}`
+  }
+}) 
 
 const likeButton = document.querySelectorAll(".like-button");
 document.body.addEventListener("click", addLikeOrComment);
 
 function addLikeOrComment(event) {
-  // console.log(event.target);
   if (event.target.className === "like-button") {
-    // function addLikeOrComment() {
-      // console.log("clicked")
-      // const likeCount = likes.textContent
-      const likeCount = parseInt(event.target.parentElement.firstElementChild.textContent.split(" ")[0])
-      event.target.parentElement.firstElementChild.textContent = `${likeCount + 1} likes`
-      // }
-      // select span through event
-      // update likes
-    } else if (event.target.className === "comment-button") {
-      // debugger
-      // console.log("commented");
-      const commentInput = document.querySelectorAll("#comment")
-      const commentsList = event.target.parentElement.previousElementSibling.firstElementChild
-      event.target.parentElement.previousElementSibling.firstElementChild.value = commentsList.li
-      // get comment input
-      // add it to the dom
-      // commentForm.addEventListener('submit', (e) => {
-      event.preventDefault()
-      const li = document.createElement("li")
-      li.textContent = commentsList.value
-      commentInput.appendChild(li)
-      e.target.reset()
-    }
-    // }
+    const likeCount = parseInt(event.target.parentElement.firstElementChild.textContent.split(" ")[0])
+    event.target.parentElement.firstElementChild.textContent = `${likeCount + 1} likes`
+  } else if (event.target.className === "comment-button") {
+    const commentText = event.target.parentElement.firstElementChild.value
+    const commentsList = event.target.parentElement.previousElementSibling
+    // get comment input
+    // add it to the dom
+    const commentLi = document.createElement("li")
+    commentLi.textContent = commentText
+    commentsList.append(commentLi)
+    event.target.parentElement.firstElementChild.value = ""
+  }
 }
 
 document.querySelectorAll('h2').forEach(h2 => h2.addEventListener('click', _ => h2.style.color = 'red'));
